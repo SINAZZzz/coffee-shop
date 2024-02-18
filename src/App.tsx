5e6f4e5db8deea6b8fac2acec8ae543bce1766a7
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-02-19 02:34:44 +0330

    add intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import Loading from "./components/loading/Loading"
 import Intro from "./pages/intro/Intro"
-
+import useWindowSize from "./util/Utility.jsx";
 
 function App() {
+  const { width } = useWindowSize();
   return (
     <>
-      {/* {width < 900 ? (
+      {width < 500 ? (
        <Intro />
       ) : (
         <Loading />
-      )} */}
-        <Loading />
+      )}
     </>
   )
 }
--- src/pages/intro/Intro.tsx
+++ src/pages/intro/Intro.tsx
@@ -1,6 +1,13 @@
+import { Box } from "@mui/material";
 
 export default function Intro() {
   return (
-    <div>intro</div>
+    <>
+    {/* img */}
+    <Box component='img' src="https://s8.uupload.ir/files/image_3_ysrq.png" alt="intro img" 
+    sx={{width:'100%'}} />
+    <Box component='img' src="https://s8.uupload.ir/files/rectangle_1_vop8.png" alt="img liner" 
+    sx={{width:'100%' , mt:'-6.2rem'}} />
+    </>
   )
 }
