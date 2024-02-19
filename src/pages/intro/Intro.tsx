8ed40340d562fcb35211958056c7f710e2484f48
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-02-19 12:26:52 +0330

    update intro background

--- src/pages/intro/Intro.tsx
+++ src/pages/intro/Intro.tsx
@@ -1,13 +1,35 @@
 import { Box } from "@mui/material";
+import Background from "./components/Background";
 
 export default function Intro() {
   return (
     <>
-    {/* img */}
-    <Box component='img' src="https://s8.uupload.ir/files/image_3_ysrq.png" alt="intro img" 
-    sx={{width:'100%'}} />
-    <Box component='img' src="https://s8.uupload.ir/files/rectangle_1_vop8.png" alt="img liner" 
-    sx={{width:'100%' , mt:'-6.2rem'}} />
+    <Box bgcolor='black' width='100%' height='100vh'>
+      {/* background */}
+      <Background />
+      {/* Title */}
+      <Box
+        component='div'
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+        }}
+      >
+        <Box
+          color="white"
+          fontSize={37}
+          fontWeight='600'
+          mx='2rem'
+          lineHeight='1.2'
+        >
+          Coffee so good, your taste buds will love it.
+        </Box>
+      </Box>
+    </Box>
     </>
   )
 }
---
+++ src/pages/intro/components/Background.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+
+export default function Background() {
+  return (
+    <>
+        <Box component='img' src="https://s8.uupload.ir/files/image_3_ysrq.png" alt="intro img" 
+        sx={{width:'100%'}} />
+    </>
+  )
+}
