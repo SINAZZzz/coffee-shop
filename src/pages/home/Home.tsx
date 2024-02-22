3ded44b412140d142ff551e60f3e4b93ff5091d2
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-02-23 01:39:10 +0330

    update home add card

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,49 @@
-import { Box, Container} from "@mui/material";
+import { Box, Card, Container, Grid, Typography} from "@mui/material";
 import Profile from "./components/Profile";
 import Search from "./components/Search";
 import Categories from "./components/Categories";
 import Banner from "./components/Banner";
+import AddIcon from '@mui/icons-material/Add';
 
+interface Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    image: string;
+  }
 
-
-
+const products: Product[] = [
+    {
+      id: 1,
+      title: "Cappucino",
+      description: "with Chocolate",
+      price: 4.53,
+      image: "https://s8.uupload.ir/files/rectangle_1706_i5d1.png"
+    },
+    {
+      id: 2,
+      title: "Espresso",
+      description: "Strong and Dark",
+      price: 3.99,
+      image: "https://s8.uupload.ir/files/rectangle_1706_i5d1.png"
+    },
+    {
+        id: 2,
+        title: "Espresso",
+        description: "Strong and Dark",
+        price: 3.99,
+        image: "https://s8.uupload.ir/files/rectangle_1706_i5d1.png"
+      },
+      {
+        id: 2,
+        title: "Espresso",
+        description: "Strong and Dark",
+        price: 3.99,
+        image: "https://s8.uupload.ir/files/rectangle_1706_i5d1.png"
+      },
+    // Add more products as needed
+  ];
 
 export default function Home() {
     
@@ -23,6 +60,42 @@ export default function Home() {
             <Container sx={{bgcolor:'#F8F6F6', height:'100vh'}}>
                 <Banner />
                 <Categories />
+      <Grid container spacing={2}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={6} sm={6} md={6} lg={6}>
+            <Card sx={{ width: '100%', display: 'flex', flexDirection: 'column', borderRadius: '20px' }}>
+              {/* img */}
+              <Box sx={{ width: '100%', my: '0.6rem' }} component='img' src={product.image} />
+              <Box mx='1rem'>
+                {/* title */}
+                <Typography fontSize='1.2rem' fontWeight='bold'>{product.title}</Typography>
+                {/* des */}
+                <Typography fontSize='1rem' color='#9B9B9B'>{product.description}</Typography>
+                {/* price */}
+                <Box display='flex' justifyContent='space-between' alignItems='center' my='1rem'>
+                  <Typography fontWeight='bold' fontSize='1.5rem'>$ {product.price.toFixed(2)}</Typography>
+                  <Box
+                    component='button'
+                    sx={{
+                      background: '#C67C4E',
+                      color: 'white',
+                      height: '2.5rem',
+                      width: '2.5rem',
+                      borderRadius: '15px',
+                      border: 'none',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center'
+                    }}
+                  >
+                    <AddIcon />
+                  </Box>
+                </Box>
+              </Box>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
             </Container>
         </Box>
     </>
