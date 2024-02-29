b38a255f151dbd277bdf5854f5aef3bce58f0271
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-02-29 12:55:12 +0330

    new update loading

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -3,23 +3,27 @@ import './style.css'
 
 export default function Loading() {
   return (
-    <Box
-    component="div"
-    display="flex"
-    flexDirection="column"
-    justifyContent="center"
-    textAlign="center"
-  >
-    <Box component="div" sx={{ fontWeight: "700", fontSize: "larger" }}>
-      <Box component="p">Loading...</Box>
+    <>
+      <Box
+      component="div" 
+      display="flex"
+      width='100%'
+      height='100vh'
+      flexDirection="column"
+      justifyContent="center"
+      textAlign="center"
+    >
+      <Box component="div" sx={{ fontWeight: "700", fontSize: "larger" }}>
+        <Box component="p">open with size mobile...</Box>
+      </Box>
+      <Box component="div" className="loading-child">
+        <Box component="div" id="square1"></Box>
+        <Box component="div" id="square2"></Box>
+        <Box component="div" id="square3"></Box>
+        <Box component="div" id="square4"></Box>
+        <Box component="div" id="square5"></Box>
+      </Box>
     </Box>
-    <Box component="div" className="loading-child">
-      <Box component="div" id="square1"></Box>
-      <Box component="div" id="square2"></Box>
-      <Box component="div" id="square3"></Box>
-      <Box component="div" id="square4"></Box>
-      <Box component="div" id="square5"></Box>
-    </Box>
-  </Box>
+  </>
   )
 }
