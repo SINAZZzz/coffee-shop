4bbdc6cc5ca71a7b4ecfb540698f8d530d01d07b
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-21 02:55:44 +0330

    update favorite icon in detail item

--- src/pages/detail-Item/Detail_Item.tsx
+++ src/pages/detail-Item/Detail_Item.tsx
@@ -1,10 +1,16 @@
 import { Box, Container, IconButton, Typography } from "@mui/material";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Detail_Item() {
   const navigate = useNavigate();
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleFavoriteClick = () => {
+    setIsFavorite(!isFavorite);
+  }
   return (
     <Box width='100%' height='100%'>
       <Container>
@@ -16,8 +22,8 @@ export default function Detail_Item() {
           <Box fontWeight='bold' fontSize='18px'>
             Detail
           </Box>
-          <IconButton> 
-            <FavoriteBorderIcon />
+          <IconButton onClick={handleFavoriteClick}> 
+            <FavoriteIcon style={{ color: isFavorite ? 'red' : 'inherit' }} />
           </IconButton>
         </Box>
         {/* image */}
