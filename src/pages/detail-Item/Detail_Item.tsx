d50c722683ca224c1d00e13449f7d46f8d850b92
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-21 15:53:19 +0330

    new update in like item product

--- src/pages/detail-Item/Detail_Item.tsx
+++ src/pages/detail-Item/Detail_Item.tsx
@@ -1,8 +1,22 @@
 import { Box, Container, IconButton, Typography } from "@mui/material";
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+
+const PinkHeartIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill='red' width="24px" height="24px">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+  </svg>
+);
+
+const PinkHeartIconWithBorder = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="white" stroke="gray" strokeWidth="2"/>
+  </svg>
+);
+
 
 export default function Detail_Item() {
   const navigate = useNavigate();
@@ -23,7 +37,7 @@ export default function Detail_Item() {
             Detail
           </Box>
           <IconButton onClick={handleFavoriteClick}> 
-            <FavoriteIcon style={{ color: isFavorite ? 'red' : 'inherit' }} />
+            {isFavorite ? <PinkHeartIcon /> : <PinkHeartIconWithBorder />}
           </IconButton>
         </Box>
         {/* image */}
