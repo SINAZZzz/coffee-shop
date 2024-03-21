de478a85f30d2119a195320f8e399f003ec79c3a
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-21 17:56:58 +0330

    add  descriptionProduct

--- src/pages/detail-Item/Detail_Item.tsx
+++ src/pages/detail-Item/Detail_Item.tsx
@@ -48,15 +48,28 @@ export default function Detail_Item() {
         <Box>
           {/* title */}
           <Typography fontSize='1.3rem' fontWeight='bold'>Cappucino</Typography>
-          {/* description */}
-          <Typography fontSize='0.8rem' color='#9B9B9B' mt='5px'>with Chocolate</Typography>
-          {/* rank */}
-          <Box display='flex' overflow='hidden' mt='10px'>
-            <Box component='img'  src="https://s8.uupload.ir/files/furnitur-icon_bg1w.png" width='20px' mr='5px' />
-            <Box component='p' fontSize='1rem' fontWeight='bold' pr='5px'>4.8</Box>
-            <Typography fontSize='0.8rem' color='#9B9B9B'>(230)</Typography>
+          <Box display='flex' justifyContent='space-between' alignItems='center'>
+            <Box>
+              {/* description */}
+              <Typography fontSize='0.8rem' color='#9B9B9B' mt='5px'>with Chocolate</Typography>
+              {/* rank */}
+              <Box display='flex' overflow='hidden' mt='10px'>
+                <Box component='img'  src="https://s8.uupload.ir/files/furnitur-icon_bg1w.png" width='20px' mr='5px' />
+                <Box component='p' fontSize='1rem' fontWeight='bold' pr='5px'>4.8</Box>
+                <Typography fontSize='0.8rem' color='#9B9B9B'>(230)</Typography>
+              </Box>
+            </Box>
+            {/* option */}
+            <Box>
+                <Box component='img'  src="https://s8.uupload.ir/files/bean_ll.png" width='20px' mr='10px' borderRadius='10px' bgcolor='#F4F4F4' p='8px' />
+                <Box component='img'  src="https://s8.uupload.ir/files/milk_v5oq.png" width='20px' borderRadius='10px' bgcolor='#F4F4F4' p='8px' />
+              </Box>
           </Box>
+          <Box component='hr' my='15px' color='#EAEAEA' bgcolor='#EAEAEA' height='0.5px' border='1px #EAEAEA solid ' borderRadius='40px' />
           {/* descriptionProduct */}
+          <Typography fontSize='1rem' fontWeight='bold'>Description</Typography>
+          <Typography fontSize='0.8rem' color='#9B9B9B' mt='5px'>A cappuccino is an approximately 150 ml (5 oz) beverage, with 25 ml of espresso coffee and 85ml of fresh milk the fo.. 
+          <Box component='span' color='#C67C4E' fontWeight='bold'>Read More</Box></Typography>
           {/* size */}
           {/* price */}
           {/* btn buy */}
