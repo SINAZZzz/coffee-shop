c78edeb1f0946273a237d8100a6e412edcaef32d
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-22 15:09:10 +0330

    new update detail

--- src/pages/detail-Item/components/detail/Detail.tsx
+++ src/pages/detail-Item/components/detail/Detail.tsx
@@ -1,8 +1,9 @@
-import { Box, Typography } from "@mui/material"
+import { Box } from "@mui/material"
 import Title from "../Title";
 import Size from "./components/size/Size";
 import DescriptionProduct from "./components/descriptionProduct/DescriptionProduct";
 import Hr from "./components/Hr";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
 
 export default function Detail() {
 
@@ -10,24 +11,7 @@ export default function Detail() {
   return (
     <Box>
         <Title fontSize={1.3} title="Cappucino" />
-
-        <Box display='flex' justifyContent='space-between' alignItems='center'>
-            <Box>
-                {/* description */}
-                <Typography fontSize='0.8rem' color='#9B9B9B' mt='5px'>with Chocolate</Typography>
-                {/* rank */}
-                <Box display='flex' overflow='hidden' mt='10px'>
-                <Box component='img'  src="https://s8.uupload.ir/files/furnitur-icon_bg1w.png" width='20px' mr='5px' />
-                <Box component='p' fontSize='1rem' fontWeight='bold' pr='5px'>4.8</Box>
-                <Typography fontSize='0.8rem' color='#9B9B9B'>(230)</Typography>
-                </Box>
-            </Box>
-            {/* option */}
-            <Box>
-                <Box component='img'  src="https://s8.uupload.ir/files/bean_ll.png" width='20px' mr='10px' borderRadius='10px' bgcolor='#F4F4F4' p='8px' />
-                <Box component='img'  src="https://s8.uupload.ir/files/milk_v5oq.png" width='20px' borderRadius='10px' bgcolor='#F4F4F4' p='8px' />
-            </Box>
-        </Box>
+        <ProductDetail />
         <Hr />
         <DescriptionProduct />
         <Size />
---
+++ src/pages/detail-Item/components/detail/components/ProductDetail/components/Option.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+
+export default function Option() {
+  return (
+   <>
+    <Box>
+        <Box component='img'  src="https://s8.uupload.ir/files/bean_ll.png" width='20px' mr='10px' borderRadius='10px' bgcolor='#F4F4F4' p='8px' />
+        <Box component='img'  src="https://s8.uupload.ir/files/milk_v5oq.png" width='20px' borderRadius='10px' bgcolor='#F4F4F4' p='8px' />
+    </Box>
+   </>
+  )
+}
