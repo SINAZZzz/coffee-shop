8060f0f162d34c516fd8c1b0492389955f7dbf9c
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-28 02:17:40 +0330

    update responsive in price product

--- src/pages/home/components/productCard/components/Price.tsx
+++ src/pages/home/components/productCard/components/Price.tsx
@@ -1,20 +1,23 @@
 import { Box, Typography } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import { priceType } from '../../../../../types/priceType';
+import useWindowSize from "../../../../../functions/Utility";
 
 
 export default function Price({price} : priceType) {
+  const { width } = useWindowSize();
+
   return (
     <Box display='flex' justifyContent='space-between' alignItems='center' my='1rem'>
-        <Typography fontWeight='bold' fontSize='1.5rem'>$ {price.toFixed(2)}</Typography>
+        <Typography fontWeight='bold' sx={{fontSize:width > 375 ? '1.5rem' : '1rem'}}>${price.toFixed(2)}</Typography>
         <Box
             component='button'
             sx={{
               background: '#C67C4E',
               color: 'white',
-              height: '2.5rem',
-              width: '2.5rem',
-              borderRadius: '15px',
+              height: width > 375 ? '2.5rem' : '2rem',
+              width: width > 375 ? '2.5rem' : '2rem',
+              borderRadius: width > 375 ? '15px' : '12px',
               border: 'none',
               display: 'flex',
               justifyContent: 'center',
