dbeb087741328fbb1d194dc10b1de4f451f87999
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-28 03:23:56 +0330

    update rank in product

--- src/pages/home/components/productCard/components/Rank.tsx
+++ src/pages/home/components/productCard/components/Rank.tsx
@@ -5,7 +5,8 @@ import { RankType } from '../../../../../types/RankType';
 
 export default function Rank({rank} : RankType) {
   return (
-    <Box bgcolor='rgba(0,0,30,0.4)' color='white' sx={{ backdropFilter: 'blur(3px)' }} mt='0.3rem' ml='0.3rem' display='flex' width='4rem' justifyContent='center' borderRadius='20px 0px 20px 0px' alignItems='center' height='2rem' position='absolute'>
+    <Box bgcolor='rgba(0,0,30,0.4)' color='white' sx={{ backdropFilter: 'blur(3px)' }} mt='4.8px' ml='4.8px'
+     display='flex' width='4rem' justifyContent='center' borderRadius='15px 0px 20px 0px' alignItems='center' height='2rem' position='absolute'>
         <Box component='img' src="https://s8.uupload.ir/files/furnitur-icon_bg1w.png" width='0.8rem' mr='5px' />
         <Box component='p' fontSize='12px' fontWeight='bold'>{rank}</Box>
     </Box>
