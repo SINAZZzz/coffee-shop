d7137296ce0d3c64151ab31ab53d0e699e356fd0
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-28 03:50:17 +0330

    update nav in production

--- src/pages/detail-Item/components/navigation/Navigation.tsx
+++ src/pages/detail-Item/components/navigation/Navigation.tsx
@@ -2,8 +2,11 @@ import { BottomNavigation } from '@mui/material'
 import Price from './components/Price'
 import { useNavigate } from "react-router-dom";
 import Btn from '../../../../components/Btn';
+import useWindowSize from '../../../../functions/Utility';
 export default function Navigation({price} : {price : number}) {
   const navigate = useNavigate();
+  const { width } = useWindowSize();
+
 
   const nextPage = () =>{
     navigate('/order')
@@ -13,7 +16,7 @@ export default function Navigation({price} : {price : number}) {
         <BottomNavigation
             sx={{
                 width: "100%",
-                position: "fixed",
+                position: "fixed", 
                 bottom: 0,
                 borderRadius:'1rem 1rem 0rem 0rem',
                 height:'5rem',
@@ -24,7 +27,7 @@ export default function Navigation({price} : {price : number}) {
                 }}
                 showLabels>
                     <Price price={price}/>
-                    <Btn Title='Buy Now' bgcolor='C67C4E' color='white' px={25} onClick={nextPage} />
+                    <Btn Title='Buy Now' py={width > 767 ? 2.5 : 4.5} fontSize='1.1rem' borderRadius='15px' border='none' bgcolor='C67C4E' color='white' px={20} onClick={nextPage} />
         </BottomNavigation>
     </>
   )
