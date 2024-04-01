d8c956733c1abfe5c1bf8fcb7995033684e561b3
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-04-01 16:52:11 +0330

    add map in Delivery

--- src/pages/delivery/Delivery.tsx
+++ src/pages/delivery/Delivery.tsx
@@ -1,8 +1,6 @@
 import { Box, Container, IconButton } from "@mui/material";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import MyLocationOutlinedIcon from '@mui/icons-material/MyLocationOutlined';
-import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { useNavigate } from "react-router-dom";
 
 export default function Delivery() {
@@ -11,21 +9,7 @@ export default function Delivery() {
     return (
         <Box position="relative" width="100%" height="100vh">
             <Box position="absolute" zIndex="-1" width="100%" height="100%">
-                <Box component="img" width="100%" height="100%" src="https://s8.uupload.ir/files/maps_o86d.png" />
-            </Box>
-
-            <Box position="absolute"  width="100%" height="100%" >
-                <Box component="img" width='46%' height='19%' src="https://s8.uupload.ir/files/vector_2_2yyk.png" mt='8.5rem' ml='5.6rem' />
-            </Box>
-            <Box position="absolute"  width="100%" height="100%" >
-                <IconButton sx={{ bgcolor: 'white', p: '0.8rem' , mt:'16rem' ,  ml:'16.2rem' }}>
-                    <DirectionsBikeIcon fontSize="small" style={{color:'#C67C4E'}} />
-                </IconButton>
-            </Box>
-            <Box position="absolute"  width="100%" height="100%" >
-                <IconButton sx={{mt:'10.5rem' ,  ml:'4.5rem' }}>
-                    <LocationOnIcon fontSize="small" style={{color:'#C67C4E'}} />
-                </IconButton>
+                <Box component="img" width="100%" height="100%" src="https://s8.uupload.ir/files/group_3159_wiw5.png" />
             </Box>
 
             <Box position="relative" zIndex="1" width="100%" height="100%">
