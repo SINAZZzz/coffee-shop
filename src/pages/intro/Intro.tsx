a054d700786d922fbded00437c5f485bd02df606
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-05-12 00:29:17 +0330

    fix: color back, add gradient to image.

--- src/pages/intro/Intro.tsx
+++ src/pages/intro/Intro.tsx
@@ -8,26 +8,52 @@ import { useNavigate } from "react-router-dom";
 export default function Intro() {
   const navigate = useNavigate();
   const NextPage = () => {
-    navigate('/home')
-  }
-
+    navigate("/home");
+  };
 
   return (
     <>
-    <Box bgcolor='black' width='100%' height='100vh'>
-      <Background /> 
-      <Title fontSize={2} title="Coffee so good, your taste buds will love it." color="white" mx='2rem' textAlign="center" />
-      <Description color="A9A9A9" fontSize="1.1rem" mx="3rem" my="1rem" textAlign="center"
-       description="The best grain, the finest roast, the powerful flavor." />
-      <Box component='div' sx={{textAlign: 'center' ,  width: "100%",
-        position: "fixed", 
-        bottom: 0,
-        left: 0,
-        right: 0,mb:'1rem'}} >
-        <Btn Title="Get Started" bgcolor="C67C4E" fontSize="1.5rem" color="white" onClick={NextPage} width="90%" py={7} borderRadius="20px" />
+      <Box bgcolor="black" width="100%" height="100vh">
+        <Background />
+        <Title
+          fontSize={2}
+          title="Coffee so good, your taste buds will love it."
+          color="white"
+          mx="2rem"
+          textAlign="center"
+        />
+        <Description
+          color="A9A9A9"
+          fontSize="1.1rem"
+          mx="3rem"
+          my="1rem"
+          textAlign="center"
+          description="The best grain, the finest roast, the powerful flavor."
+        />
+        <Box
+          component="div"
+          sx={{
+            textAlign: "center",
+            width: "100%",
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            mb: "1rem",
+          }}
+        >
+          <Btn
+            Title="Get Started"
+            bgcolor="C67C4E"
+            fontSize="1.5rem"
+            color="white"
+            onClick={NextPage}
+            width="90%"
+            py={7}
+            borderRadius="20px"
+          />
+        </Box>
       </Box>
-    </Box>
     </>
-  ) 
+  );
 }
- 
--- src/pages/intro/components/Background.tsx
+++ src/pages/intro/components/Background.tsx
@@ -3,8 +3,39 @@ import { Box } from "@mui/material";
 export default function Background() {
   return (
     <>
-        <Box component='img' src="https://s8.uupload.ir/files/image_3_ysrq.png" alt="intro img" 
-        sx={{width:'100%' , height:'50%' , objectFit:'cover' }} />
+      <Box
+        component="div"
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "50%",
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src="https://s8.uupload.ir/files/image_3_ysrq.png"
+          alt="intro img"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+            height: "50%",
+            background:
+              "linear-gradient(to bottom, rgba(255, 255, 255, 0), #000000)",
+          }}
+        />
+      </Box>
     </>
-  )
+  );
 }
